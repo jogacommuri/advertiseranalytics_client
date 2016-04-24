fb03578ad989471eccc84c1cada83e0d93cf1006
joga commuri <jogacommuri@yahoo.com>
2016-04-23 18:44:05 -0700

    graphs done

--- public/app/controllers/eventCtrl.js
+++ public/app/controllers/eventCtrl.js
@@ -1,246 +1,63 @@
 angular.module('eventCtrl',[])
-    .controller('eventCtrl',function($scope,$window){
+    .controller('eventCtrl',function($scope,$window,$http){
     var event =  this;
     $scope.userDetails = JSON.parse($window.localStorage.getItem('user'));
     event.profileData = {};
     event.profileData =  $scope.userDetails;
+     /*event.data= [];*/
+    event.get_instate_count = function(){
+        $http.post('http://advanalytics.herokuapp.com/geo_instate_count')
+            .success(function(data){
+            console.log("post sucess");
+            console.log(data);
+             event.data = data.results;
+           /* console.log(event.data);*/
+            event.displayMap(event.data);
+        })
+        .error(function(data) {
+				console.log('Error: ' + data);
+			});
+    }
+  
     
-    event.title = "event";
-    event.state = "us-ca";
-    event.number = 100;
-
-     event.data = [{
-    "hc-key": "us-ma",
-    "event": 'Number of events',
-    "value": 44
-  }, {
-    "hc-key": "us-wa",
-    "event": 'Number of events',
-      "value": 1
-  }, {
-    "hc-key": event.state,
-    "event": 'Number of events',
-    "value": event.number
-  }, {
-    "hc-key": "us-or",
-      "event": 'Number of events',
-    "value": 3
-  }, {
-    "hc-key": "us-wi",
-      "event": 'Number of events',
-    "value": 4
-  }, {
-    "hc-key": "us-me",
-      "event": 'Number of events',
-    "value": 5
-  }, {
-    "hc-key": "us-mi",
-      "event": 'Number of events',
-    "value": 6
-  }, {
-    "hc-key": "us-nv",
-      "event": 'Number of events',
-    "value": 7
-  }, {
-    "hc-key": "us-nm",
-      "event": 'Number of events',
-    "value": 8
-  }, {
-    "hc-key": "us-co",
-      "event": 'Number of events',
-    "value": 9
-  }, {
-    "hc-key": "us-wy",
-      "event": 'Number of events',
-    "value": 10
-  }, {
-    "hc-key": "us-ks",
-      "event": 'Number of events',
-    "value": 11
-  }, {
-    "hc-key": "us-ne",
-      "event": 'Number of events',
-    "value": 12
-  }, {
-    "hc-key": "us-ok",
-      "event": 'Number of events',
-    "value": 13
-  }, {
-    "hc-key": "us-mo",
-      "event": 'Number of events',
-    "value": 14
-  }, {
-    "hc-key": "us-il",
-      "event": 'Number of events',
-    "value": 15
-  }, {
-    "hc-key": "us-in",
-      "event": 'Number of events',
-    "value": 16
-  }, {
-    "hc-key": "us-vt",
-      "event": 'Number of events',
-    "value": 17
-  }, {
-    "hc-key": "us-ar",
-      "event": 'Number of events',
-    "value": 18
-  }, {
-    "hc-key": "us-tx",
-      "event": 'Number of events',
-    "value": 19
-  }, {
-    "hc-key": "us-ri",
-      "event": 'Number of events',
-    "value": 20
-  }, {
-    "hc-key": "us-al",
-      "event": 'Number of events',
-    "value": 21
-  }, {
-    "hc-key": "us-ms",
-      "event": 'Number of events',
-    "value": 22
-  }, {
-    "hc-key": "us-nc",
-      "event": 'Number of events',
-    "value": 23
-  }, {
-    "hc-key": "us-va",
-      "event": 'Number of events',
-    "value": 24
-  }, {
-    "hc-key": "us-ia",
-      "event": 'Number of events',
-    "value": 25
-  }, {
-    "hc-key": "us-md",
-      "event": 'Number of events',
-    "value": 26
-  }, {
-    "hc-key": "us-de",
-      "event": 'Number of events',
-    "value": 27
-  }, {
-    "hc-key": "us-pa",
-      "event": 'Number of events',
-    "value": 28
-  }, {
-    "hc-key": "us-nj",
-      "event": 'Number of events',
-    "value": 29
-  }, {
-    "hc-key": "us-ny",
-      "event": 'Number of events',
-    "value": 30
-  }, {
-    "hc-key": "us-id",
-      "event": 'Number of events',
-    "value": 31
-  }, {
-    "hc-key": "us-sd",
-      "event": 'Number of events',
-    "value": 32
-  }, {
-    "hc-key": "us-ct",
-      "event": 'Number of events',
-    "value": 33
-  }, {
-    "hc-key": "us-nh",
-      "event": 'Number of events',
-    "value": 34
-  }, {
-    "hc-key": "us-ky",
-      "event": 'Number of events',
-    "value": 35
-  }, {
-    "hc-key": "us-oh",
-      "event": 'Number of events',
-    "value": 36
-  }, {
-    "hc-key": "us-tn",
-      "event": 'Number of events',
-    "value": 37
-  }, {
-    "hc-key": "us-wv",
-      "event": 'Number of events',
-    "value": 38
-  }, {
-    "hc-key": "us-dc",
-      "event": 'Number of events',
-    "value": 39
-  }, {
-    "hc-key": "us-la",
-      "event": 'Number of events',
-    "value": 40
-  }, {
-    "hc-key": "us-fl",
-      "event": 'Number of events',
-    "value": 41
-  }, {
-    "hc-key": "us-ga",
-      "event": 'Number of events',
-    "value": 42
-  }, {
-    "hc-key": "us-sc",
-      "event": 'Number of events',
-    "value": 43
-  }, {
-    "hc-key": "us-mn",
-      "event": 'Number of events',
-    "value": 44
-  }, {
-    "hc-key": "us-mt",
-      "event": 'Number of events',
-    "value": 45
-  }, {
-    "hc-key": "us-nd",
-      "event": 'Number of events',
-    "value": 46
-  }, {
-    "hc-key": "us-az",
-      "event": 'Number of events',
-    "value": 47
-  }, {
-    "hc-key": "us-ut",
-      "event": 'Number of events',
-    "value": 48
-  }, {
-    "hc-key": "us-hi",
-      "event": 'Number of events',
-    "value": 49
-  }, {
-    "hc-key": "us-ak",
-      "event": 'Number of events',
-    "value": 50
-  }, {
-    "value": 51
-  }];
 
   var pointClick = function() {
+        event.postData={};
+        event.postData.state = this.name;
+            $http.post('http://advanalytics.herokuapp.com/geo_instate_categoryCount',event.postData)
+                    .success(function(data){
+                    console.log("post sucess");
+                    console.log(data);
+                    event.donutData = data.results;
+                console.log(event.donutData);
+                event.donutChart(event.donutData);
+                    
+                })
+                .error(function(data) {
+                        console.log('Error: ' + data);
+                    });
                 var row = this.options.row,
                     $div = $('<div></div>')
                         .dialog({
-                            title: this.event,
-                            width: 600,
+                            title: this.name,
+                            width: 800,
                             height: 500
                         });
 
-                window.chart = new Highcharts.Chart({
+                event.donutChart =function(data){
+                  Highcharts.chart({
                    
                     chart: {
                         renderTo: $div[0],
                         type: 'pie',
-                        width: 500,
-                        height: 400,
+                        width: 600,
+                        height: 500,
                         
                          
                      
                         options3d: {
-                         enabled: true,
-                         alpha: 45 ,
-           
-                        
+                         enabled: true,       
+                        alpha:25
                       }
                     },
                     plotOptions: {
@@ -254,106 +71,83 @@ angular.module('eventCtrl',[])
                     },
                     series: [{
                         name: 'Events',
-                        data: [{
-                            name: 'Technology',
-                            value: 30,
-                             y: parseInt(30),
-                            /*color: '#00cc44',*/
-                            
-                        }, {
-                            name: 'Music',
-                            value: 20,
-                            y: parseInt(20),
-                            /*color: '#ff9933',*/
-                           
-                        },
-                        {
-                            name: 'Movies',
-                            value: 15,
-                             y: parseInt(15),
-                            /*color: '#1a75ff',*/
-                            
-                        },
-                        {
-                            name: 'Sports',
-                            value: 35,
-                             y: parseInt(35),
-                            /*color: '#e63900',*/
-                            
-                        }],
+                        data: event.donutData,
                         
                         dataLabels: {
                             format: '<b>{point.name}</b> {point.value}'
                         }
                     }]
-                });
+                });  
+                }  
             };
 
-		Highcharts.Map( {
+		event.displayMap = function(data){
+            Highcharts.Map( {
 
-      chart: {
-                        renderTo: 'container',
-                        type: 'map',
-                        height: 500,
-                        weight: 400,
-                        backgroundColor: "#eee"
+              chart: {
+                                renderTo: 'container',
+                                type: 'map',
+                                height: 600,
+                                weight: 500,
+                                backgroundColor: "#eee"
+                            },
+                         title: {
+              text: 'Events data in the US'
+            },
+
+            subtitle: {
+              text: null
+            },
+            legend: {
+                    layout: 'vertical',
+                    borderWidth: 0,
+                    backgroundColor: 'rgba(255,255,255,0.85)',
+                    floating: 'true',
+                    align:'left'
+
+                },
+            mapNavigation: {
+              enabled: true,
+              buttonOptions: {
+                verticalAlign: 'bottom'
+              }
+            },
+
+            colorAxis: {
+              min: 0
                     },
-                 title: {
-      text: 'Events data in the US'
-    },
 
-    subtitle: {
-      text: null
-    },
-    legend: {
-            layout: 'vertical',
-            borderWidth: 0,
-            backgroundColor: 'rgba(255,255,255,0.85)',
-            floating: 'true',
-            align:'left'
-            
-        },
-    mapNavigation: {
-      enabled: true,
-      buttonOptions: {
-        verticalAlign: 'bottom'
-      }
-    },
- 
-    colorAxis: {
-      min: 0
-            },
-
-    series: [{
-      data: event.data,
-      mapData: Highcharts.maps['countries/us/us-all'],
-      joinBy: 'hc-key',
-      name: 'Event data',
-      states: {
-        hover: {
-          color: '#BADA55'
-        }
-      },
-      dataLabels: {
-        enabled: true,
-        format: '{point.name}'
-      },
-        point: {
-                events: {
-                    click: pointClick
+            series: [{
+              data: event.data,
+              mapData: Highcharts.maps['countries/us/us-all'],
+              joinBy: 'hc-key',
+              name: 'Event data',
+              states: {
+                hover: {
+                  color: '#BADA55'
                 }
+              },
+              dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+              },
+                point: {
+                        events: {
+                            click: pointClick
+                        }
+                    },
+                 tooltip: {
+                        pointFormat: '{point.name}:<br> {point.event} :{point.value}'
+            }
             },
-         tooltip: {
-                pointFormat: '{point.name}:<br> {point.event} :{point.value}'
-    }
-    },
-    {
-      name: 'Separators',
-      type: 'mapline',
-      data: Highcharts.geojson(Highcharts.maps['countries/us/us-all'], 'mapline'),
-      color: 'silver',
-      showInLegend: false,
-      enableMouseTracking: false
-    }]
-				});
+            {
+              name: 'Separators',
+              type: 'mapline',
+              data: Highcharts.geojson(Highcharts.maps['countries/us/us-all'], 'mapline'),
+              color: 'silver',
+              showInLegend: false,
+              enableMouseTracking: false
+            }]
+        });
+        }
 });
--- public/app/controllers/eventDemoCtrl.js
+++ public/app/controllers/eventDemoCtrl.js
@@ -1,16 +1,74 @@
 angular.module('eventDemoCtrl',[])
-    .controller('eventDemoCtrl',function($scope,$window){
+    .controller('eventDemoCtrl',function($scope,$window,$http){
     var vm =  this;
     $scope.userDetails = JSON.parse($window.localStorage.getItem('user'));
     $scope.trendings = JSON.parse($window.localStorage.getItem('Interests'));
     vm.profileData = {};
     vm.profileData =  $scope.userDetails;
+    
+    vm.eventName ={};
+    vm.maleData = [];
+    vm.femaleData = [];
+    vm.locations = [];
+    vm.eventCount = [];
+    //event get
+        vm.getCategory =  function(){
+            console.log("getcat");
+            console.log(vm.eventName);
+            vm.postData = {};
+            vm.postData.category_id = vm.eventName.category_id;
+            $http.post('http://advanalytics.herokuapp.com/demographics_age', vm.postData)
+            .success(function(data){
+                console.log("post sucess");
+                //console.log(data.male);
+                for(var key in data.male){
+                    vm.maleData.push(-data.male[key]);
+                }
+               
+            
+                for(var key in data.female){
+                    vm.femaleData.push(data.female[key]);
+                }
+                //console.log(vm.femaleData);
+                 //event.data = data.results;
+               /* console.log(event.data);*/
+                //event.displayMap(event.data);
+                vm.displayAgeChart(vm.maleData, vm.femaleData);
+            })
+            .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+            
+            
+            vm.categoryDetails = {};
+            vm.categoryDetails.category = vm.eventName.category_id;
+             $http.post('http://advanalytics.herokuapp.com/getEventCountsByCategory', vm.categoryDetails)
+            .success(function(data){
+                console.log("post sucess");
+                console.log(data.eventCounts);
+                for(var i=0; i< data.eventCounts.length;i++){
+                    vm.locations.push(data.eventCounts[i].region_name);
+                    vm.eventCount.push(data.eventCounts[i].noOfEvents);
+                }
+                console.log(vm.locations);
+                 console.log(vm.eventCount);
+                vm.displayLocationChart(vm.locations, vm.eventCount);
+            })
+            .error(function(data) {
+                    console.log('Error: ' + data);
+                });
+            
+        }
     vm.categories = ['18-24', '25-34', '35-44', '45-54',
             '55-64', '65+'];
+    
+   
 
-    vm.locations = ['CA', 'AZ', 'WI','CO','NY','FL'];
 
-    Highcharts.chart('container_age', {
+    
+    vm.displayAgeChart = function(maleData, femaleData){
+        
+         Highcharts.chart('container_age', {
 
     	chart: {
     		    type: 'bar'
@@ -60,12 +118,54 @@ angular.module('eventDemoCtrl',[])
 
             series: [{
                 name: 'Male',
-                data: [-10.2, -2.2, -2.3, -2.5, -2.7, -3.1]
+                data: maleData
             }, {
                 name: 'Female',
-                data: [2.1, 2.0, 2.2, 2.4, 2.6, 3.0]
+                data: femaleData
             }]
         });
+        
+    }
+
+   
+    vm.displayLocationChart = function(locations, count){
+        
+        
+      Highcharts.chart({
+        chart: {
+        	renderTo: 'container_location',
+            type: 'column',
+            options3d: {
+                enabled: false,
+                alpha: 10,
+                beta: 25,
+                depth: 70
+            }
+        },
+        title: {
+            text: 'Events in Locations'
+        },
+       
+        plotOptions: {
+            column: {
+                depth: 50
+            }
+        },
+        xAxis: {
+            categories: locations
+        },
+        yAxis: {
+            title: {
+                text: null
+            }
+        },
+        series: [{
+            name: 'States',
+            data: count
+        }]
+    });
+        
+    }
 
 
      Highcharts.chart('container_cross', {
@@ -78,7 +178,7 @@ angular.module('eventDemoCtrl',[])
             		enabled: false
             },
             title: {
-                text: 'Geographics'
+                text: 'Cross Device'
             },
             tooltip: {
                 pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -115,39 +215,6 @@ angular.module('eventDemoCtrl',[])
 				});
 
 
-      Highcharts.chart({
-        chart: {
-        	renderTo: 'container_location',
-            type: 'column',
-            options3d: {
-                enabled: true,
-                alpha: 10,
-                beta: 25,
-                depth: 70
-            }
-        },
-        title: {
-            text: 'Events in Locations'
-        },
-       
-        plotOptions: {
-            column: {
-                depth: 25
-            }
-        },
-        xAxis: {
-            categories: vm.locations
-        },
-        yAxis: {
-            title: {
-                text: null
-            }
-        },
-        series: [{
-            name: 'States',
-            data: [2, 3, null, 4, 0, 5]
-        }]
-    });
     });
     
 
