36a4f20a55461beeae8db27e1879c7bab705189d
santoshsibyala <santoshsibyala@gmail.com>
2016-05-09 21:02:12 -0700

    getting nike events

--- public/app/controllers/analyticsCtrl.js
+++ public/app/controllers/analyticsCtrl.js
@@ -1,10 +1,13 @@
-angular.module('analyticsCtrl',[])
+angular.module('analyticsCtrl',['angularUtils.directives.dirPagination'])
     .controller('analyticsCtrl',function($scope,$http,$window,$location){
     var vm = this;
     vm.title = "Analytics";
     vm.token={};
     vm.fname=null;
-    vm.getCategories=function(token){
+    vm.both=true;
+    vm.eventsnike=null;
+    vm.nikeevents=false;
+    vm.getCategories=function(){
        vm.token.token =$window.localStorage.getItem('accessToken');
         console.log(vm.token.token);
         $http.post("http://advanalytics.herokuapp.com/users/getUser",vm.token)
@@ -17,5 +20,27 @@ angular.module('analyticsCtrl',[])
             .error(function(data) {
 				console.log('Error: ' + data);
 			});
-    };               
+    }; 
+    vm.nike=function(){
+        $http.post("http://advanalytics.herokuapp.com/get_nike")
+            .success(function(data){
+                vm.eventsnike=data.events;
+                vm.both=false; 
+                vm.nikeevents=true;  
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+
+    };
+    vm.toyota=function(){
+        $http.post("http://advanalytics.herokuapp.com/get_toyota")
+            .success(function(data){
+                vm.both=false;
+                alert('success');
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };              
 });
